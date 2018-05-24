23cfb837969c11daccce3169cda3516d71dbd095
Ally Netzel <netzelally@gmail.com>
2018-05-24 14:13:53 +0000

    automatically backed up by learn

--- app/show.rb
+++ app/show.rb
@@ -1,11 +1,13 @@
 class Show < ActiveRecord::Base
-  # def self.highest_rating
-  #   Shows.maximum("rating")
-  # end
+  def self.highest_rating
+    Shows.maximum("rating")
+  end
 
 
 
   def self.lowest_rating
     Shows.minimum("rating")
   end
+
+  
 end
