b29d62a8b93cf2a875427aa01ce37b6e1d0fcedd
Ally Netzel <netzelally@gmail.com>
2018-05-24 13:54:53 +0000

    Done.

--- app/show.rb
+++ app/show.rb
@@ -1,9 +1,11 @@
 class Show < ActiveRecord::Base
-  def highest_rating
-    Shows.maximum("rating")
-<<<<<<< HEAD
-=======
+  # def self.highest_rating
+  #   Shows.maximum("rating")
+  # end
 
->>>>>>> a069b0a0c17a2a6e74f0602f7103bce5fc760718
+
+
+  def self.lowest_rating
+    Shows.minimum("rating")
   end
 end
